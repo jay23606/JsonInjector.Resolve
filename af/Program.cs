d79cb39a990ff3bbc0c4d730ec88e2c9a6f287f8
jay23606 <jay_23606@yahoo.com>
2022-04-12 09:27:35 -0400

    null checks in extension method

--- af/Program.cs
+++ af/Program.cs
@@ -29,25 +29,25 @@ namespace DemoApp
             //USING CreateInstance
             var co = Activator.CreateInstance(typeof(ConsoleOutput)) as IOutput;
             var instance = Activator.CreateInstance(typeof(TodayWriter), co) as IDateWriter;
-            instance.WriteDate();
+            instance?.WriteDate();
 
             //USING CreateInstance with parameters array
             List<object> parameters = new List<object>();
             co = Activator.CreateInstance(typeof(ConsoleOutput)) as IOutput;
             parameters.Add(co);
             instance = Activator.CreateInstance(typeof(TodayWriter), parameters.ToArray()) as IDateWriter;
-            instance.WriteDate();
+            instance?.WriteDate();
 
 
             //USING CreateInstance and Type.GetType (so that parameters could be stored in xml,json for example)
-            Type type = Type.GetType("DemoApp.ConsoleOutput, af");
+            Type? type = Type.GetType("DemoApp.ConsoleOutput, af");
             co = Activator.CreateInstance(type) as IOutput;
             type = Type.GetType("DemoApp.TodayWriter, af");
             parameters = new List<object>();
             parameters.Add(co);
             //instance = Activator.CreateInstance(type, co) as IDateWriter;
             instance = Activator.CreateInstance(type, parameters.ToArray()) as IDateWriter;
-            instance.WriteDate();
+            instance?.WriteDate();
 
 
             //we could look up the parameters from the fully qualified name here in a dictionary
@@ -71,12 +71,15 @@ namespace DemoApp
             //We could instantiate objects without constructor parameters first
             foreach (var inst in instances)
             {
-                if (inst.Parameters.Count == 0 && !nameInstance.ContainsKey(inst.Name))
+                if (inst.Name == null) continue;
+                if (inst.Parameters?.Count == 0 && !nameInstance.ContainsKey(inst.Name))
                 {
                     var myType = Type.GetType(inst.Name);
-                    var myInst = Activator.CreateInstance(myType);
-                    //myInst = Convert.ChangeType(myInst, myType);
-                    nameInstance.Add(inst.Name, myInst);
+                    if (myType != null)
+                    {
+                        var myInst = Activator.CreateInstance(myType);
+                        if(myInst != null) nameInstance.Add(inst.Name, myInst);
+                    }
                 }
             }
 
@@ -144,7 +147,7 @@ namespace DemoApp
             //So in this case we want to look up the instance based on the interface name
             //
             //Added a recursive call to Resolve so if TodayWriter, for example, is passed
-            //into another object's ctor--I have
+            //into another object's ctor, it will get instantiated
             json = @"
 [
     {
@@ -179,23 +182,24 @@ namespace DemoApp
     //can we create a helper class to do this for us?
     public static class DI
     {
-        public static Dictionary<string, object> Resolve(this string json, Dictionary<string, object> nameInstance = null)
+        public static Dictionary<string, object> Resolve(this string json, Dictionary<string, object>? nameInstance = null)
         {
             json = json.Replace("'", "\"");
 
             var instances = JsonSerializer.Deserialize<List<Instance>>(json);
-            //Dictionary<string, object>
             if (nameInstance == null) nameInstance = new Dictionary<string, object>();
 
-            if(instances==null) return nameInstance;
+            if (instances == null) return nameInstance;
             //We could instantiate objects without constructor parameters first
             foreach (var inst in instances)
             {
                 if(inst.Parameters == null && inst.Interface != null)
                 {
+                    if (inst.Name == null) continue;
                     //lets check what actually happens in these scenarios
                     var myType = Type.GetType(inst.Name);
-                    var ctors = myType?.GetConstructors();
+                    if (myType == null) continue;
+                    var ctors = myType.GetConstructors();
                     if (ctors != null && ctors.Length > 0)
                     {
                         foreach (var ctor in ctors)
@@ -203,6 +207,7 @@ namespace DemoApp
                             if (ctor.GetParameters().Length == 0 && !nameInstance.ContainsKey(inst.Interface))
                             {
                                 var myInst = Activator.CreateInstance(myType);
+                                if (myInst == null) continue;
                                 nameInstance.Add(inst.Interface, myInst);
                             }
                         }
@@ -216,7 +221,7 @@ namespace DemoApp
                 }
                 else if (inst?.Parameters?.Count == 0)
                 {
-
+                    if (inst.Name == null) continue;
                     var myType = Type.GetType(inst.Name);
                     if (myType != null && !nameInstance.ContainsKey(inst.Name))
                     {
@@ -231,6 +236,7 @@ namespace DemoApp
             {
                 if (inst.Parameters == null && inst.Interface != null)
                 {
+                    if (inst.Name == null) continue;
                     var myType = Type.GetType(inst.Name);
                     var ctors = myType?.GetConstructors();
                     if (ctors != null && ctors.Length > 0)
@@ -256,6 +262,7 @@ namespace DemoApp
                 }
                 else if (inst?.Parameters?.Count > 0)
                 {
+                    if (inst.Name == null) continue;
                     var myType = Type.GetType(inst.Name);
 
                     var myParameters = new List<object>();
@@ -289,9 +296,9 @@ namespace DemoApp
 
     public class Instance
     {
-        public string Name { get; set; }
-        public string Interface { get; set; }
-        public List<string> Parameters { get; set; }
+        public string? Name { get; set; }
+        public string? Interface { get; set; }
+        public List<string>? Parameters { get; set; }
     }
 
     public interface IOutput
